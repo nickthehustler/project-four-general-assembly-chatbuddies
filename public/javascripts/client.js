b5ac7bf09389e0489a0c95f1b68f92d485f7de34
Nick Brown <nicholas.g.brown@gmail.com>
2016-06-07 10:49:01 -0400

    client.js => Added signup and login controller along with setting up my ui-routes for my signup/login process...renamed home.ejs, login.ejs, error.ejs, index.ejs, signup.ejs to home.html, login.html, index.html, error.html and signup.html...modified the home.html file changed the ui-view= to just ui-view.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,8 +1,13 @@
 angular.module('chatBuddies', ['ui.router']);
 
 angular.module('chatBuddies')
-.controller('AuthCtrl', function(){
+.controller('signupCtrl', function(){
+  console.log("signupCtrl is alive!");
+});
 
+angular.module('chatBuddies')
+.controller('loginCtrl', function() {
+  console.log("loginCtrl is alive!");
 });
 
 
@@ -10,4 +15,21 @@ angular.module('chatBuddies')
 .config(function($stateProvider, $urlRouterProvider, $locationProvider){
   $urlRouterProvider.otherwise("/home");
   $locationProvider.html5Mode(true);
+
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "views/home.html"
+    })
+    .state('signup', {
+      url: "/signup",
+      templateUrl: "views/signup.html",
+      controller: "signupCtrl"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "views/login.html",
+      controller: "loginCtrl"
+    });
 });
