09465b7b31088c84b1a0a71e8197ed65c8849d7b
Nick Brown <nicholas.g.brown@gmail.com>
2016-06-07 10:21:04 -0400

    Added mention of the angular.module, a controller for authentication and a config for utilized ui-router for angular.js...I am missing the logic the controller and the config.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,2 +1,13 @@
 angular.module('chatBuddies', ['ui.router']);
 
+angular.module('chatBuddies')
+.controller('AuthCtrl', function(){
+
+});
+
+
+angular.module('chatBuddies')
+.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+  $urlRouterProvider.otherwise("/home");
+  $locationProvider.html5Mode(true);
+});
