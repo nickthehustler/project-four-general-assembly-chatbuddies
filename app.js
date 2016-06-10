8626fd1f6174395a74bf8bb525a8f9ffcfe84f40
Nick Brown <nicholas.g.brown@gmail.com>
2016-06-10 11:53:43 -0400

    Added serve-favicon module.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // required modules
 var path            = require('path');
-// var favicon         = require('serve-favicon');
+var favicon         = require('serve-favicon');
 var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
@@ -22,15 +22,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use('/', homeRoutes);
 app.use('/api/users', userRoutes);
 
